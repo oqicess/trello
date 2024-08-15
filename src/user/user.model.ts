3f442fdab1aa6064818033e9a26e1b7375c59617
Andrey <oqicess@mail.ru>
2024-08-16 01:28:29 +0300

    chore(user): fix type in model

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,5 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Table, Column, Model, DataType } from 'sequelize-typescript';
+import { Table, Column, Model, DataType, HasMany } from 'sequelize-typescript';
+import { Comments } from '../comments/comments.model';
+import { Columns } from '../column/column.model';
 
 interface UserCreateAttrs {
     email: string;
@@ -26,19 +28,25 @@ export class User extends Model<User, UserCreateAttrs> {
         type: DataType.STRING,
         allowNull: false,
     })
-    email: number;
+    email: string;
 
     @ApiProperty({ example: '12345678', description: 'Пароль пользователя' })
     @Column({
         type: DataType.STRING,
         allowNull: false,
     })
-    password: number;
+    password: string;
 
     @ApiProperty({ example: 'IvanovIvan', description: 'Имя пользователя' })
     @Column({
         type: DataType.STRING,
         allowNull: false,
     })
-    name: number;
+    name: string;
+
+    @HasMany(() => Columns)
+    columns: Columns[];
+
+    @HasMany(() => Comments)
+    comments: Comments[];
 }
