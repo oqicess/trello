ad813bb29f98023c7c6ac540a94f91b5a90aa5b8
Andrey <oqicess@mail.ru>
2024-08-15 22:50:54 +0300

    swagger and env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function start() {
+    const PORT = process.env.PORT || 8001;
+    const docs = new DocumentBuilder()
+        .setTitle('Тестовое задание')
+        .setDescription('Описание проекта')
+        .addTag('github: oqicess')
+        .addBearerAuth()
+        .build();
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    
+    const document = SwaggerModule.createDocument(app, docs);
+    SwaggerModule.setup('/api/docs', app, document);
+
+    app.enableCors({
+        origin: ['http://localhost:8080'],
+        credentials: true,
+    });
+
+    await app.listen(PORT, ()=> console.log(`Server started on port = ${PORT}`));
 }
+
 start();
