bde7b12ee34b0524b6f6ce8ea6a49e812c5d77f3
Andrey <oqicess@mail.ru>
2024-08-17 15:37:13 +0300

    chore(register): add validation

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,12 +1,16 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class RegisterUserDto {
     @ApiPropertyOptional({ example: 'oqicess@trello.ru' })
+    @IsString({ message: 'Должно быть строкой' })
     readonly email: string;
 
     @ApiPropertyOptional({ example: '12345678' })
+    @IsString({ message: 'Должно быть строкой' })
     readonly password: string;
 
     @ApiPropertyOptional({ example: 'oqicess' })
+    @IsString({ message: 'Должно быть строкой' })
     readonly name: string;
 }
